b1e42b9b9bf168b353379550f80a0fe948eceaaf
Sudar Meruva <meruva_s@yaho.com>
2020-03-05 06:15:27 -0500

    Created heroku JawsDB MySQL DB, table, inserted data and updated connections.js

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,14 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 // create connection to db
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PW,
-  database: process.env.DB_NAME
-});
+const connection = process.env.JAWSDB_URL
+  ? mysql.createConnection(process.env.JAWSDB_URL)
+  : mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
+  });
 
 module.exports = connection;
